bf9e6d888a3cdf190cbb956308b5bff5fb3fd529
zfj <893298592@qq.com>
2021-02-25 21:33:30 +0800

    switch to training

--- source_code/main.py
+++ source_code/main.py
@@ -59,7 +59,7 @@ def init_modules():
     options = {}
 
     options["is_debugging"] = False
-    options["is_predicting"] = True 
+    options["is_predicting"] = False 
     options["model_selection"] = False # When options["is_predicting"] = True, true means use validation set for tuning, false is real testing.
 
     options["cuda"] = cfg.CUDA and torch.cuda.is_available()
@@ -509,7 +509,7 @@ def run(existing_model_name = None):
         predict_model = False
 
     # 加载模型
-    need_load_model = True
+    need_load_model = False
     existing_model_name = 'cnndm.s2s.gru.gpu9.epoch5.2'
 
     print_basic_info(modules, consts, options)
